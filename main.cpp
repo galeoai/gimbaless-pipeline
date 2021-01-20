53a0deb6cb4cdc6d34533b67db6c728cb94b1d85
Or Dicker <or.dicker@gmail.com>
2021-01-20 15:09:15 +0200

    some code cleanup

--- main.cpp
+++ main.cpp
@@ -26,78 +26,53 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 
 int main(int argc, char *argv[])
 {
-    printf("Hello!\n");
-    cv::Mat image,image1;
-    image = cv::imread(argv[1], cv::IMREAD_ANYDEPTH);
-    image1 = cv::imread(argv[2], cv::IMREAD_ANYDEPTH);
+    // load images 
+    cv::Mat image1,image2;
+    image1 = cv::imread(argv[1], cv::IMREAD_ANYDEPTH);
+    image2 = cv::imread(argv[2], cv::IMREAD_ANYDEPTH);
     
-    if ( !image.data )
+    if ( !image1.data && !image2.data )
     {
-        printf("No image data \n");
+        printf("No images data \n");
         return -1;
     }
-    auto im = image.ptr<uint8_t>(0);
-
-    int *im_gpu;
-    cudaHostRegister(im, image.total() * image.elemSize() , cudaHostRegisterMapped);
-    gpuErrchk(cudaHostGetDevicePointer((void **)&im_gpu, (void *)im, 0));
-    Halide::Runtime::Buffer<uint8_t> input(nullptr, image.rows,image.cols);
-    input.device_wrap_native(halide_cuda_device_interface(),(uintptr_t)im_gpu);
-    input.set_host_dirty();
-
+    // image1 => halide buffer with zerocopy
     auto im1 = image1.ptr<uint8_t>(0);
-
     int *im1_gpu;
     cudaHostRegister(im1, image1.total() * image1.elemSize() , cudaHostRegisterMapped);
     gpuErrchk(cudaHostGetDevicePointer((void **)&im1_gpu, (void *)im1, 0));
-    Halide::Runtime::Buffer<uint8_t> input1(nullptr, image1.rows,image1.cols);
-    input1.device_wrap_native(halide_cuda_device_interface(),(uintptr_t)im1_gpu);
-    input1.set_host_dirty();
-
+    Halide::Runtime::Buffer<uint8_t> h_image1(nullptr, image1.rows,image1.cols);
+    h_image1.device_wrap_native(halide_cuda_device_interface(),(uintptr_t)im1_gpu);
+    h_image1.set_host_dirty();
     
-    cv::Mat image_out = cv::Mat::zeros(image.rows,image.cols,image.type()); 
-    //cv::Mat image_out = cv::Mat::zeros(image.rows/32,image.cols/32,CV_32SC1);
-    auto im_out = image_out.ptr<uint8_t>(0);
+    // image2 => halide buffer with zerocopy
+    auto im2 = image1.ptr<uint8_t>(0);
+    int *im2_gpu;
+    cudaHostRegister(im2, image2.total() * image2.elemSize() , cudaHostRegisterMapped);
+    gpuErrchk(cudaHostGetDevicePointer((void **)&im2_gpu, (void *)im2, 0));
+    Halide::Runtime::Buffer<uint8_t> h_image2(nullptr, image2.rows,image2.cols);
+    h_image2.device_wrap_native(halide_cuda_device_interface(),(uintptr_t)im2_gpu);
+    h_image2.set_host_dirty();
 
+    //output buffer
+    cv::Mat image_out = cv::Mat::zeros(image1.rows,image1.cols,image1.type()); 
+    auto im_out = image_out.ptr<uint8_t>(0);
     int *im_out_gpu;
     cudaHostRegister(im_out, image_out.total() * image_out.elemSize() , cudaHostRegisterMapped);
     gpuErrchk(cudaHostGetDevicePointer((void **)&im_out_gpu, (void *)im_out, 0));
     
     Halide::Runtime::Buffer<uint8_t> output(nullptr, image_out.rows,image_out.cols);
     output.device_wrap_native(halide_cuda_device_interface(),(uintptr_t)im_out_gpu);
-    
-    
-    
-    //for (auto i=0; i < 100; ++i) {
-    // 	gpu_only(input,input, output);
-    //}
 
+    // benchmark
     double auto_schedule_off = Halide::Tools::benchmark(2, 5, [&]() {
-        
-	gpu_only(input,input1, output);
+	gpu_only(h_image1,h_image2, output);
 	output.device_sync();
     });
     printf("Manual schedule: %gms\n", auto_schedule_off * 1e3);
-    // Verify output.
-    //for (int y = 0; y < image.cols; y++) {
-    // 	for (int x = 0; x < image.rows; x++) {
-    // 	    if (im[x+y*image.rows] * 3 !=  im_out[x+y*image.rows]) {
-    // 		printf("Error at %d, %d: 3*%d != %d\n", x, y, im[x+y*image.rows], im_out[x+y*image.rows]);
-    // 		return -1;
-    // 	    }
-    // 	}
-    //}
-    //for (auto i=0; i<image_out.cols; ++i) { 
-    // 	for (auto j=0; j<image_out.rows; ++j) { 
-    // 	    std::cout << im_out[i+j*image_out.rows] << ", ";
-    // 	}
-    // 	std::cout << "\n";
-    //}
-    //std::cout << "cols: " << image_out.cols << "\n";
-    //std::cout << "rows: " << image_out.rows << "\n";
 
     cv::imwrite("out.tiff", image_out);
-    std::cout << "Success!" << "\n";
+    std::cout << "Done!" << "\n";
 
     return 0;
 }
