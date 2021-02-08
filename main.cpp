21fa74dd00f58df0b5c6f24adb94b1b56dc3cc00
Or Dicker <or.dicker@gmail.com>
2021-02-08 11:06:16 +0200

    gstream using h264 over UDP

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <opencv2/core/utility.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
 #include <stdio.h>
 #include "opencv2/opencv.hpp"
 
@@ -21,7 +22,8 @@ int main(int argc, char *argv[])
     cv::Mat image1,image2;
     image1 = cv::imread(argv[1], cv::IMREAD_ANYDEPTH);
     image2 = cv::imread(argv[2], cv::IMREAD_ANYDEPTH);
-    cv::Mat image_out = cv::Mat::zeros(image1.rows,image1.cols,image1.type()); 
+    //cv::Mat image_out = cv::Mat::zeros(image1.rows,image1.cols,image1.type());
+    cv::Mat image_out = cv::Mat::zeros(1024,1024,image1.type()); 
     //std::vector<cv::String> filenames;
     //cv::glob(strcat(argv[3],"/*.tif"), filenames);
     //std::vector<cv::Mat> images;
@@ -40,14 +42,32 @@ int main(int argc, char *argv[])
     auto output	  = Zerocopy::gpu<uint8_t>(image_out);
 
     // benchmark
-    double auto_schedule_off = Halide::Tools::benchmark(2, 5, [&]() {
-	process(h_image1,h_image2, output);
-	output.device_sync();
-    });
-    printf("Manual schedule: %gms\n", auto_schedule_off * 1e3);
+    //double auto_schedule_off = Halide::Tools::benchmark(2, 5, [&]() {
+    // 	process(h_image1,h_image2, output);
+    // 	output.device_sync();
+    //});
+    //printf("Manual schedule: %gms\n", auto_schedule_off * 1e3);
+    // 
+    //cv::imwrite("out.tiff", image_out);
 
-    cv::imwrite("out.tiff", image_out);
-    std::cout << "Done!" << "\n";
+    cv::VideoWriter out;
+    out.open("appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=10.4.20.12 port=5000 ",
+	     0,
+	     16,
+	     cv::Size (1024, 1024),
+	     false);
+    
+    out.write(image_out);
+    while (true) {
+	//cv::imshow("output",image_out);
+	//out.write(image_out);
+	cv::randu(image_out, 0,255);
+	out.write(image1);
+	char c=(char)cv::waitKey(25);
+	if(c==27)
+	    break;
+    }
+    //std::cout << "Done!" << "\n";
 
     return 0;
 }
